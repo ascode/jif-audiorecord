cdb5c106b0aab64e1c42ffad4867fd5c3da17add
jinfei <ascode@icloud.com>
2015-12-26 11:49:35 +0800

    init

---
+++ jif-audiorecord/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  jif-audiorecord
+//
+//  Created by 金飞 on 15/12/26.
+//  Copyright © 2015年 Fei Jin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
